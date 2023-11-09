786c5710df5567969f1ac4de2b78bce80296b052
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-11-09 13:15:22 -0600

    inital react/vite app created

---
+++ README.md
@@ -0,0 +1,5 @@
+# LifeCycle Simulation
+
+The goal of this project is to explore and create a basic simulation of lifeforms using particles.
+
+
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      LifeCycle Simulation
+    </>
+  )
+}
+
+export default App
